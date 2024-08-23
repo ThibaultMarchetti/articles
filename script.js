8984d6385283d8a958b7c750d50b2db6e3afc248
Marchetti Thibault <Thibault.marchetti1@gmail.com>
2024-08-23 09:30:08 +0200

    Premier commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Ajoutez des fonctionnalités interactives si nécessaire
+document.addEventListener('DOMContentLoaded', function () {
+    // Exemple de script pour une future fonctionnalité
+    console.log("Le site est chargé et prêt !");
+});
